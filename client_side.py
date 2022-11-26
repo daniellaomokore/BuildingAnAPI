45e097803cf93e525470a45f8e5c2a2957f749c1
Daniella Omokore <daniellaomokore@gmail.com>
2022-11-26 00:51:36 +0000

    Commit

--- client_side.py
+++ client_side.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+""" These all show the functions that carry out the HTTP request methods (POST,PUT,DELETE) to the API """
+""" We don't need GET here as GET is automatically run by default whenever we just load the URL endpoints """
 
 #POST REQUEST
 new_flight = {
@@ -16,6 +18,7 @@ new_flight = {
       "flight_id": 777
 }
 
+# lines 22-25 below shows us creating a HTTP POST request with headers and body
 headers = {'content-type': 'application/json'}
 result = requests.post(
     'http://127.0.0.1:5000/flights', headers=headers, data=json.dumps(new_flight)
@@ -23,7 +26,11 @@ result = requests.post(
 print(result)
 
 
-# # PUT REQUEST
+
+
+
+
+# PUT REQUEST
 updated_flight = {
      "new content": "test",
      "flight_id": 555
@@ -31,6 +38,7 @@ updated_flight = {
 
 fid = 555
 
+# lines 44-47 below shows us creating a HTTP PUT request with headers and body
 headers = {'content-type': 'application/json'}
 result = requests.put(
     'http://127.0.0.1:5000/flights/{}'.format(fid), headers=headers, data=json.dumps(updated_flight)
@@ -38,10 +46,15 @@ result = requests.put(
 print(result)
 
 
+
+
+
+
 # DELETE REQUEST
 
 fid = 555
 
+# lines 60-63 below shows us creating a HTTP DELETE request with headers and body
 headers = {'content-type': 'application/json'}
 result = requests.delete(
     'http://127.0.0.1:5000/flights/{}'.format(fid), headers=headers
