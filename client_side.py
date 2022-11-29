cd00235365148aab4f19bea9a1f926cc0368355d
Daniella Omokore <daniellaomokore@gmail.com>
2022-11-29 03:19:18 +0000

    commit

--- client_side.py
+++ client_side.py
@@ -5,6 +5,7 @@ import json
 """ We don't need GET here as GET is automatically run by default whenever we just load the URL endpoints """
 
 # NOTE run each request type separately by commenting out the other to show how the all work on the endpoint correctly
+# NOTE POST & PUT requests use request body as they are sending info. GET and DELETE do not use request body
 
 # POST REQUEST
 new_flight = {
@@ -21,9 +22,10 @@ new_flight = {
 }
 
 # lines 22-25 below shows us creating a HTTP POST request with headers and body
-headers = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json',
+           'Authorization': '[your API key]'}
 result = requests.post(
-    'http://127.0.0.1:5000/flights', headers=headers, data=json.dumps(new_flight)
+    'http://127.0.0.1:5000/flights', headers=headers, data=json.dumps(new_flight),
 )
 print(result)
 
@@ -39,7 +41,8 @@ updated_flight = {
 fid = 555
 
 # lines 44-47 below shows us creating a HTTP PUT request with headers and body
-headers = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json',
+           'Authorization': '[your API key]'}
 result = requests.put(
     'http://127.0.0.1:5000/flights/{}'.format(fid), headers=headers, data=json.dumps(updated_flight)
 )
@@ -54,7 +57,8 @@ print(result)
 fid = 555
 
 # lines 60-63 below shows us creating a HTTP DELETE request with headers and body
-headers = {'content-type': 'application/json'}
+headers = {'content-type': 'application/json',
+           'Authorization': '[your API key]'}
 result = requests.delete(
     'http://127.0.0.1:5000/flights/{}'.format(fid), headers=headers
 )
